061a2aeb9e679260e702df915551a1b094bd8f23
Lorris brosset <sb290@live.com>
2020-02-24 19:02:52 +0100

    Changement du bot

---
+++ config.js
@@ -0,0 +1,2 @@
+exports.TOKEN = "NjgxNTM0NjQ2Nzg1NTQwMTEz.XlQLLw.3SOgrPdy9CAzBqO7YwwHvaoVVjE";
+exports.PREFIX = "f!";
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const { TOKEN, PREFIX } = require("./config");
 const client = new Discord.Client({ disableEveryone: true});
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log("Je suis prêt!");
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') msg.channel.send('Pong!');
-  if (msg.content === 'everyone') msg.channel.send("@everyone, Salut tout le monde!", { disableEveryone: false});
-  if (msg.content === 'noteveryone') msg.channel.send("@everyone (noteveryone), Salut tout le monde!",);
+client.on("message", msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping!");
+  if (msg.content === "everyone") msg.channel.send("@everyone, Salut tout le monde!", { disableEveryone: false});
+  if (msg.content === "noteveryone") msg.channel.send("@everyone (noteveryone), Salut tout le monde!",);
 });
 
-client.login('NjgxNTM0NjQ2Nzg1NTQwMTEz.XlQCrg.2mAbYkS3FTHS5zF-RI75IO52Y10');
+client.login(TOKEN);
